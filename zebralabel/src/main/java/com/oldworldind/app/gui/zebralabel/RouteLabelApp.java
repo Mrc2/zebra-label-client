3cb5981768336805b465df5eaf014f134ba7f442
Mike <mcolegrove@oldworldind.com>
2012-12-28 17:26:35 -0600

    setup initial app for label

---
+++ zebralabel/src/main/java/com/oldworldind/app/gui/zebralabel/RouteLabelApp.java
@@ -0,0 +1,13 @@
+package com.oldworldind.app.gui.zebralabel;
+
+/**
+ * Hello world!
+ *
+ */
+public class RouteLabelApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zebralabel/src/test/java/com/oldworldind/app/gui/zebralabel/RouteLabelAppTest.java
@@ -0,0 +1,38 @@
+package com.oldworldind.app.gui.zebralabel;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RouteLabelAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RouteLabelAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RouteLabelAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
