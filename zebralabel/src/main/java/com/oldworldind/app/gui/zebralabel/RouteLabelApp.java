c62c5b18ce39fe0f0f1be9427a37bc212a99fa2f
Mike <mcolegrove@oldworldind.com>
2012-12-29 18:08:36 -0600

    more too

--- zebralabel/src/main/java/com/oldworldind/app/gui/zebralabel/RouteLabelApp.java
+++ zebralabel/src/main/java/com/oldworldind/app/gui/zebralabel/RouteLabelApp.java
@@ -7,19 +7,32 @@ import javax.swing.SwingUtilities;
  * @since Dec 15, 2012 at 1:26:44 AM
  * @author mcolegrove
  */
-public class RouteLabelApp
-{
+public class RouteLabelApp {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         displayUsage();
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createPickandRunLabelPanel();
+            }
+        });
     }
 
+    private static void createPickandRunLabelPanel() {
+         JFrame jf = new JFrame("Label Router");
 
-    /**
-     * @TODO - Provide a brief description of the required and optional arguments for this application
+
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jf.pack();
+        jf.setVisible(true);
+    }
+
+    /** 
      *
      */
     private static void displayUsage() {
