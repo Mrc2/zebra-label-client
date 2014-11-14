4cb49603113f0b11457f0642b19cdd896c1c22d8
mcolegrove <mcolegrove@jharrisone6430.oldworldind.com>
2014-11-14 01:06:55 -0600

    ugrade dependencies to jdk 1.8 - show properties on start up in log

--- zebralabel/src/main/java/com/oldworldind/app/gui/zebralabel/RouteLabelApp.java
+++ zebralabel/src/main/java/com/oldworldind/app/gui/zebralabel/RouteLabelApp.java
@@ -1,8 +1,9 @@
 package com.oldworldind.app.gui.zebralabel;
 
+import java.util.Map;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-
 import org.apache.log4j.Logger;
 
 /**
@@ -19,6 +20,7 @@ public class RouteLabelApp {
         displayUsage();
         System.out.println("RouteLabelApp!");
         LOG.info("RouteLabelApp started");
+        logProperties(System.getenv());
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -28,7 +30,7 @@ public class RouteLabelApp {
     }
 
     private static void createPickandRunLabelPanel() {
-         JFrame jf = new JFrame("Route Label App");
+        JFrame jf = new JFrame("Route Label App");
         jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 //        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   not preferred in an applet
 
@@ -39,11 +41,21 @@ public class RouteLabelApp {
         jf.setVisible(true);
     }
 
-    /** 
+    /**
      *
      */
     private static void displayUsage() {
         System.out.println("RouteLabelApp users parameters as seen below");
         System.out.println("java RouteLabelApp <input File to Route>  <device>");
+    }
+
+    private static void logProperties(Map<String, String> env) {
+        LOG.info("Start of Runtime Properties");
+        env.entrySet().stream().forEach((entrySet) -> {
+            String key = entrySet.getKey();
+            String value = entrySet.getValue();
+            LOG.info("key:" + key + "[" + value + "]");
+        });
+        LOG.info("End of Runtime Properties");
     }
 }
